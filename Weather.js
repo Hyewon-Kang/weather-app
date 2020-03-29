276ad5e564d568303ea23ae4d66c307f93943220
Hyewon-Kang <gpdnjs22@@>
2020-03-29 21:45:40 +0900

    #2.5

--- Weather.js
+++ Weather.js
@@ -1,18 +1,92 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { StyleSheet } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-export default function Weather({ temp }) {
+const weatherOptions = {
+    Haze: {
+        iconName: "weather-fog",
+        gradient: ["#D7D2CC", "#304352"],
+        title: "Haze",
+        subtitle: "Just don't go outside"
+    },
+    Thunderstorm: {
+        iconName: "weather-lightning",
+        gradient: ["#373B44", "#4286f4"],
+        title: "Thunderstorm in the house",
+        subtitle: "Actually, outside of the house"
+    },
+    Drizzle: {
+        iconName: "weather-hail",
+        gradient: ["#2c3e50", "#2980b9"],
+        title: "Drizzle",
+        subtitle: "Is like rain, but gay 🏳️‍🌈"
+    },
+    Rain: {
+        iconName: "weather-pouring",
+        gradient: ["#000428", "#004e92"],
+        title: "It's raining outside",
+        subtitle: "Bring an umbrella"
+    },
+    Snow: {
+        iconName: "weather-snowy",
+        gradient: ["#2C3E50", "#4CA1AF"],
+        title: "Cold as balls",
+        subtitle: "Do you want to build a snowman? Fuck no."
+    }, 
+    Atmosphere: {
+        iconName: "weather-hail",
+        gradient: ["#89F7FE", "#66A6FF"],
+        title: "",
+        subtitle: ""
+
+    },
+    Clear: {
+        iconName: "weather-sunny",
+        gradient: ["#FF7300", "#FEF253"],
+        title: "Sunny as fuck",
+        subtitle: "Go get your ass burnt"
+    },
+    Clouds: {
+        iconName: "weather-cloudy",
+        gradient: ["#D7D2CC", "#304352"],
+        title: "Clouds",
+        subtitle: "I know, fucking boring"
+    },
+    Mist: {
+        iconName: "weather-hail",
+        gradient: ["#4DA0B0", "#D39D38"],
+        title: "Mist!",
+        subtitle: "It's like you have no glasses on."
+    },
+    Dust: {
+        iconName: "weather-fog",
+        gradient: ["#0f0c29", "#302b63","#24243e"],
+        title: "Dusty",
+        subtitle: "Wear a mask when you go out"
+    }
+};
+
+export default function Weather({ temp, condition }) {
     return (
-        <View stlye={styles.container}>
+        <LinearGradient
+            colors={weatherOptions[condition].gradient}
+            style={styles.container}
+        >
+            <StatusBar barStyle="light-content"/>
             <View style={styles.halfContainer}>
-                <MaterialCommunityIcons size={96} name="weather-lightning-rainy"/>
-            </View>
-            <View style={styles.halfContainer}>
+                <MaterialCommunityIcons 
+                    size={96} 
+                    name={weatherOptions[condition].iconName}
+                    color="white"/>
                 <Text style={styles.temp}>{temp}°C</Text>
             </View>
-        </View>
+            <View style={styles.textContainer}>
+                <Text style={styles.title}>{weatherOptions[condition].title}</Text>
+                <Text style={styles.subtitle}>{weatherOptions[condition].subtitle}</Text>
+            </View>
+        </LinearGradient>
     );
 }
 
@@ -44,6 +118,26 @@ const styles = StyleSheet.create({
         alignItems: "center"
     },
     temp: {
-        fontSize: 36
+        fontSize: 42,
+        color: "white"
+    },
+    title: {
+        fontSize: 44,
+        fontWeight: "300",
+        color: "white",
+        marginBottom: 10,
+        textAlign: "left"
+    },
+    subtitle: {
+        fontWeight: "600",
+        color: "white",
+        fontSize: 24,
+        textAlign: "left"
+    },
+    textContainer: {
+        alignItems: "flex-start",
+        paddingHorizontal: 40,
+        justifyContent: "center",
+        flex: 1
     }
 });
